a6402bbe5d6a58312ce639f48483f21363321db1
akarpenko-segterra <119593273+akarpenko-segterra@users.noreply.github.com>
2024-04-30 18:43:50 +0200

    initial settings

--- Tradernet/ViewController.swift
+++ Tradernet/ViewController.swift
@@ -5,15 +5,24 @@
 //  Created by Alexander Karpenko on 29.04.2024.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let container = UIView()
+        container.backgroundColor = .lightGray
+        let label = UILabel()
+        label.text = "Hey World!"
+
+        container.addSubview(label)
+
+        label.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+
+        view = container
     }
-
-
 }
-
